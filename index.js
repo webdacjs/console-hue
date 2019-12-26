1fceccdd1bf5e4ccabccea445cc5325b0a80ae66
Juan Convers <me@jco.im>
2019-12-26 20:17:28 +0100

    Fixing console log issue in babel.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const {log} = require('console')
 const exportFns = {}
 const getColorMsg = require('./colormsg.js')
 const msgTypes = require('./msgTypes')()
 
 function logMsg (type) {
-    return function (msg, original) {
-        log(getColorMsg(type, msg, original))
-    }
+  return function (msg, original) {
+    console.log(getColorMsg(type, msg, original))
+  }
 }
 
 Object.keys(msgTypes).map(k => exportFns[k] = logMsg(k))
